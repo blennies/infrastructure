92bfe7afc09a0d62b6f80ba2c5c7d5736795ce6f
Blennies <blennies.dev@gmail.com>
2025-08-17 17:32:36 +0100

    style: fix formatting for `tofu fmt`

--- modules/gh_repository/main.tf
+++ modules/gh_repository/main.tf
@@ -72,53 +72,53 @@ resource "github_issue_labels" "default" {
   repository = github_repository.default.name
 
   label {
-      color       = "0075ca"
-      description = "Improvements or additions to documentation"
-      name        = "documentation"
+    color       = "0075ca"
+    description = "Improvements or additions to documentation"
+    name        = "documentation"
   }
   label {
-      color       = "008672"
-      description = "Extra attention is needed"
-      name        = "help wanted"
+    color       = "008672"
+    description = "Extra attention is needed"
+    name        = "help wanted"
   }
   label {
-      color       = "388f7b"
-      description = "Improvements or additions to the CI/CD flow"
-      name        = "ci/cd"
+    color       = "388f7b"
+    description = "Improvements or additions to the CI/CD flow"
+    name        = "ci/cd"
   }
   label {
-      color       = "7057ff"
-      description = "Good for newcomers"
-      name        = "good first issue"
+    color       = "7057ff"
+    description = "Good for newcomers"
+    name        = "good first issue"
   }
   label {
-      color       = "a2eeef"
-      description = "New feature or request"
-      name        = "enhancement"
+    color       = "a2eeef"
+    description = "New feature or request"
+    name        = "enhancement"
   }
   label {
-      color       = "cfd3d7"
-      description = "This issue or pull request already exists"
-      name        = "duplicate"
+    color       = "cfd3d7"
+    description = "This issue or pull request already exists"
+    name        = "duplicate"
   }
   label {
-      color       = "d73a4a"
-      description = "Something isn't working"
-      name        = "bug"
+    color       = "d73a4a"
+    description = "Something isn't working"
+    name        = "bug"
   }
   label {
-      color       = "d876e3"
-      description = "Further information is requested"
-      name        = "question"
+    color       = "d876e3"
+    description = "Further information is requested"
+    name        = "question"
   }
   label {
-      color       = "e4e669"
-      description = "This doesn't seem right"
-      name        = "invalid"
+    color       = "e4e669"
+    description = "This doesn't seem right"
+    name        = "invalid"
   }
   label {
-      color       = "ffffff"
-      description = "This will not be worked on"
-      name        = "wontfix"
+    color       = "ffffff"
+    description = "This will not be worked on"
+    name        = "wontfix"
   }
 }
