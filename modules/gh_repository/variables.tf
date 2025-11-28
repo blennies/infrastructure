2a89eea20c55df6e6cf29e1e349d98107e7ce850
Blennies <blennies.dev@gmail.com>
2025-11-28 16:03:10 +0000

    feat: add `homepage_url` option to projects/repos to specify a linked website (#16)
    chore: add existing homepage URL to the unbikit project
    
    chore: update description of the unbikit repo
    
    chore: update provider versions

--- modules/gh_repository/variables.tf
+++ modules/gh_repository/variables.tf
@@ -1,23 +1,29 @@
 variable "name" {
   type        = string
-  description = "Name of the repository."
+  description = "Name of the repository/project."
   nullable    = false
 }
 
 variable "description" {
   type        = string
-  description = "Short human-readable summary about the repository."
+  description = "Short human-readable summary about the repository/project."
+  default     = null
+}
+
+variable "homepage_url" {
+  type        = string
+  description = "URL of the website associated with the project."
   default     = null
 }
 
 variable "enable_gh_pages" {
   type        = bool
-  description = "Whether to enable the creation of a GitHub Pages website for this repository."
+  description = "Whether to enable the creation of a GitHub Pages website for this project."
   default     = false
 }
 
 variable "topics" {
   type        = set(string)
-  description = "Set of topics that apply to the repository."
+  description = "Set of topics that apply to the repository/project."
   default     = null
 }
